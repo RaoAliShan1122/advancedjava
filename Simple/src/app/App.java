e3edc4f642f5554a69b15498b5b94a82145d2b11
Rao Ali Shan <raoalishan750@gmail.com>
2022-05-13 19:10:57 +0500

    i am changing myself

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -6,6 +6,7 @@ public class App{
         System.out.println("i want to go");
         App.gone();
         System.out.println("i am working with test branch");
+        System.out.println("i am commiting changes");
     } 
 public static void gone(){
     System.out.println("it is the gone method");
