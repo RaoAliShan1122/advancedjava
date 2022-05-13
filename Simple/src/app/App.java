77b91cbeb8af2c3873198a15a61e8b3c84289020
Rao Ali Shan <raoalishan750@gmail.com>
2022-05-13 18:43:56 +0500

    addedd

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -5,6 +5,7 @@ public class App{
         System.out.println("go to hell");
         System.out.println("i want to go");
         App.gone();
+        System.out.println("i am working with test branch");
     } 
 public static void gone(){
     System.out.println("it is the gone method");
