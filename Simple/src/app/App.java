4682cc2efe25b39563921a5e251c7a03adb2ba33
raoalishan750@gmail.com <raoalishan750@gmail.com>
2022-05-14 16:04:38 +0500

    shani

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -7,6 +7,7 @@ public class App{
         App.gone();
         System.out.println("i am working with test branch");
         System.out.println("i am commiting changes");
+        System.out.println("go there is nothing");
     } 
 public static void gone(){
     System.out.println("it is the gone method");
